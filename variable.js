5c912d0d7185d83e6e0a49749b02686fe069dc4a
SuneelKum12345 <snkmr231@gmail.com>
2024-02-22 09:53:52 +0530

    Added index

---
+++ variable.js
@@ -0,0 +1,16 @@
+let num = 10;
+var num1 = 10;
+const num2 = 11.5;
+num =15;
+num1 =20;
+
+console.log("This is number:" + num)
+console.log("This is javascript log:" + num)
+console.log("variable:" + num1)
+console.log("Matrix:" + num2)
+console.log("Font value:"+ num2)
+
+console.table(num, num1, num2);
+
+
+
