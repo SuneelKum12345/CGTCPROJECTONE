e2385927ca57d3c0ae3fc91d5b69a6893494ffc2
SuneelKum12345 <snkmr231@gmail.com>
2024-02-22 10:21:21 +0530

    Added index page

--- variable.js
+++ variable.js
@@ -9,8 +9,33 @@ console.log("This is javascript log:" + num)
 console.log("variable:" + num1)
 console.log("Matrix:" + num2)
 console.log("Font value:"+ num2)
+console.log("Font value:"+ num2)
+console.log("Font value:"+ num2)
+console.log("Font value:"+ num2)
 
 console.table(num, num1, num2);
+console.table(num, num1, num2);
+console.table(num, num1, num2);
+console.table(num, num1, num2);
+console.table(num, num1, num2);
+console.warn(num, num1, num2);
+console.assert(num, num1, num2);
 
+let name = 'Mukul';
+console.log(name); 
+name = 'Rahul';
+console.log(name); 
+let org_name;
+org_name = "GeeksforGeeks";
+console.log(org_name); 
 
+const arrowFunc = () => {
+    console.log("Declared using the arrow functions");
+    console.log("We are learning about arrow Function")
+}
+arrowFunc();
 
+const name1 = 'GeeksforGeeks';
+console.log('Printed by using Template Literal');
+console.log(`My name is ${name1}`);
+ 
